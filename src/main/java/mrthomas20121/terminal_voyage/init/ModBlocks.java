0d2481e012845f2cea3d7cda54458f56cd3498cc
Mrthomas20121 <thomas77300@gmail.com>
2022-03-29 07:17:59 +0200

    Made sand block actually support gravity

--- src/main/java/mrthomas20121/terminal_voyage/init/ModBlocks.java
+++ src/main/java/mrthomas20121/terminal_voyage/init/ModBlocks.java
@@ -22,9 +22,9 @@ public class ModBlocks {
     public static final Pair<RegistryObject<Block>, RegistryObject<Item>> SILICON_BRONZE_BLOCK = registerMetalBlock("copper_silicon_block");
     public static final Pair<RegistryObject<Block>, RegistryObject<Item>> REDSTONE_ALLOY_BLOCK = registerMetalBlock("redstone_alloy_block");
 
-    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> STRANGE_AQUAMARINE_SHALE_ORE = registerSandBlock("strange_aquamarine_shale_ore");
-    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> CRYSTAL_AQUAMARINE_SHALE_ORE = registerSandBlock("crystal_aquamarine_shale_ore");
-    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> MIDNIGHT_AQUAMARINE_SHALE_ORE = registerSandBlock("midnight_aquamarine_shale_ore");
+    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> STRANGE_AQUAMARINE_SHALE_ORE = registerSandBlock("strange_aquamarine_shale_ore", 0xD5B791);
+    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> CRYSTAL_AQUAMARINE_SHALE_ORE = registerSandBlock("crystal_aquamarine_shale_ore", 16777215);
+    public static final Pair<RegistryObject<Block>, RegistryObject<Item>> MIDNIGHT_AQUAMARINE_SHALE_ORE = registerSandBlock("midnight_aquamarine_shale_ore", 3553084);
 
     public static final Pair<RegistryObject<Block>, RegistryObject<Item>> LUNAR_DIMENSIONAL_SHARD_ORE = registerOreBlock("lunar_dimensional_shard_ore");
     public static final Pair<RegistryObject<Block>, RegistryObject<Item>> TURQUOISE_DIMENSIONAL_SHARD_ORE = registerOreBlock("turquoise_dimensional_shard_ore");
@@ -40,8 +40,8 @@ public class ModBlocks {
         return register(name, new Block(AbstractBlock.Properties.of(material)));
     }
 
-    private static Pair<RegistryObject<Block>, RegistryObject<Item>> registerSandBlock(String name) {
-        return register(name, new Block(AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND).requiresCorrectToolForDrops().harvestTool(ToolType.SHOVEL).strength(2.0F, 2.0F)));
+    private static Pair<RegistryObject<Block>, RegistryObject<Item>> registerSandBlock(String name, int color) {
+        return register(name, new SandBlock(color, AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND).requiresCorrectToolForDrops().harvestTool(ToolType.SHOVEL).strength(2.0F, 2.0F)));
     }
 
     private static Pair<RegistryObject<Block>, RegistryObject<Item>> registerOreBlock(String name) {
