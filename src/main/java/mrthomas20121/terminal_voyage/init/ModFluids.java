8c798a4ac9f2a9cec53cf1d7b3c16c817cfecb58
Mrthomas20121 <thomas77300@gmail.com>
2022-04-02 21:34:10 +0200

    0.0.6 update

--- src/main/java/mrthomas20121/terminal_voyage/init/ModFluids.java
+++ src/main/java/mrthomas20121/terminal_voyage/init/ModFluids.java
@@ -13,16 +13,17 @@ public class ModFluids {
 
     public static FluidDeferredRegister FLUIDS = new FluidDeferredRegister(TerminalVoyage.MOD_ID);
 
-    public static FluidObject<ForgeFlowingFluid> REDSTONE_ALLOY = FLUIDS.register("redstone_alloy", hotBuilder("redstone_alloy").temperature(1200), Material.LAVA, 7);
-    public static FluidObject<ForgeFlowingFluid> SILICON_BRONZE = FLUIDS.register("copper_silicon", hotBuilder("copper_silicon").temperature(1500), Material.LAVA, 7);
-    // we do not need to register steel as tinkers is already registering it for mod compat.
-    //public static FluidObject<ForgeFlowingFluid> STEEL = FLUIDS.register("steel", hotBuilder("steel").temperature(1800), Material.LAVA, 7);
+    public static FluidObject<ForgeFlowingFluid> REDSTONE_ALLOY = FLUIDS.register("redstone_alloy", hotBuilder("redstone_alloy").temperature(2000), Material.LAVA, 7);
+    public static FluidObject<ForgeFlowingFluid> SILICON_BRONZE = FLUIDS.register("copper_silicon", hotBuilder("copper_silicon").temperature(2200), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> CERTUS_QUARTZ = FLUIDS.register("certus_quartz", hotBuilder("certus_quartz").temperature(1400), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> CHARGED_CERTUS_QUARTZ = FLUIDS.register("charged_certus_quartz", hotBuilder("charged_certus_quartz").temperature(2500), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> fluix = FLUIDS.register("fluix", hotBuilder("fluix").temperature(2000), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> sky_stone = FLUIDS.register("sky_stone", hotBuilder("sky_stone").temperature(1200), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> SILICON = FLUIDS.register("silicon", hotBuilder("silicon").temperature(2000), Material.LAVA, 7);
     public static FluidObject<ForgeFlowingFluid> LIMESTONE = FLUIDS.register("limestone", hotBuilder("limestone").temperature(1000), Material.LAVA, 7);
+    public static FluidObject<ForgeFlowingFluid> NEBU = FLUIDS.register("nebu", hotBuilder("nebu").temperature(2500), Material.LAVA, 7);
+    public static FluidObject<ForgeFlowingFluid> HALLOWED_GOLD = FLUIDS.register("hallowed_gold", hotBuilder("hallowed_gold").temperature(2900), Material.LAVA, 7);
+    public static FluidObject<ForgeFlowingFluid> DEADWOOD_SAP = FLUIDS.register("deadwood_sap", hotBuilder("deadwood_sap").temperature(500), Material.WATER, 7);
 
     private static FluidAttributes.Builder hotBuilder(String name) {
         return FluidAttributes.builder(new ResourceLocation(String.format("terminal_voyage:block/fluids/%s_still", name)), new ResourceLocation(String.format("terminal_voyage:block/fluids/%s_flow", name))).density(2000).viscosity(10000).temperature(1000).sound(SoundEvents.BUCKET_FILL_LAVA, SoundEvents.BUCKET_EMPTY_LAVA);
